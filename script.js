b4ef8662f103f6e3a7d6611b278db2d7e42e288f
jkgyso <jkgyso@gmail.com>
2025-10-03 05:34:21 +0800

    feat: add new book, remove button, and toggle status features

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const bookContainer = document.querySelector('.book-container');
+const formDialog = document.querySelector('#form-dialog');
+const openDialogForm = document.querySelector('#open-dialog');
+const closeDialogForm = document.querySelector('#close-dialog');
+const addBookForm = document.querySelector('#bookForm');
 
 
 
@@ -23,8 +27,9 @@ function addBookToLibrary(author, title, pages, readStatus) {
 addBookToLibrary('John Smith', 'Javascript', 345, 'read');
 addBookToLibrary('Jane Doe', 'CSS', 123, 'not read');
 addBookToLibrary('Mikasa Ackerman', 'React', 567, 'read');
-console.log(myLibrary)
 
+console.log(myLibrary)
+console.log(myLibrary[0].readStatus)
 
 // Display books 
 const displayBooks = () => {
@@ -48,14 +53,126 @@ const displayBooks = () => {
         pages.textContent = `Pages: ${book.pages}`;
 
         const status = document.createElement('p');
-        status.classList.add('book-status');
-        status.textContent = `Status: ${book.readStatus}`;
+        status.classList.add('book-status')
+        const spanStatus = document.createElement('span');
+
+        book.readStatus === 'read' ? spanStatus.classList.add('read') : spanStatus.classList.add('not-read')
+        status.textContent = `Status: `;
+        spanStatus.textContent = `${book.readStatus}`;
+        status.appendChild(spanStatus);
+
+        
+
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-button');
+        removeBtn.textContent = `Remove`;
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(status);
+        card.appendChild(removeBtn)
     })
 }
 
-window.addEventListener('DOMContentLoaded', displayBooks)
+// Add a New Book functionality
+const addNewBook = event => {
+    event.preventDefault();
+    const title = document.querySelector('input[id=title]').value; 
+    const author = document.querySelector('input[id=author]').value;
+    const pages = document.querySelector('input[id=pages]').value;
+    const status = document.querySelector('#status-select').value;
+
+    const statusUpper = status.replace(status[0], status[0].toUpperCase());
+   
+    addBookToLibrary(author, title, pages, statusUpper);
+    renderNewBook();
+
+
+    formDialog.close();
+    console.log(myLibrary)
+
+}
+
+// Render new Book
+const renderNewBook = () => {
+    const lastBookSaved = myLibrary[myLibrary.length - 1];
+
+     const card = document.createElement('div');
+    card.classList.add('card');
+
+    bookContainer.appendChild(card);
+
+    const title = document.createElement('p');
+    title.classList.add('book-title')
+    title.textContent = lastBookSaved.title;
+    
+    const author = document.createElement('p');
+    author.classList.add('book-author');
+    author.textContent = `Author: ${lastBookSaved.author}`;
+
+    const pages = document.createElement('p');
+    pages.classList.add('book-pages');
+    pages.textContent = `Pages: ${lastBookSaved.pages}`;
+
+    const status = document.createElement('p');
+    status.classList.add('book-status')
+    const spanStatus = document.createElement('span');
+
+    lastBookSaved.readStatus === 'read' ? spanStatus.classList.add('read') : spanStatus.classList.add('not-read')
+    status.textContent = `Status: `;
+    spanStatus.textContent = `${lastBookSaved.readStatus}`;
+    status.appendChild(spanStatus);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-button');
+    removeBtn.textContent = `Remove`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(status);
+    card.appendChild(removeBtn)
+}
+
+// Remove Book 
+const removeBook = event => {
+    const book = event.target;
+    const card = document.querySelector('.card');
+    if (book.classList.contains('remove-button')) {
+        card.remove();
+    }
+}
+
+// Toggle Read status 
+const toggleStatus = event => {
+    console.log(event.target);
+    const status = event.target; 
+
+    if (status.classList.contains('read')) {
+        status.classList.add('not-read');
+        status.classList.remove('read');
+        status.textContent = 'not read';
+    } else if (status.classList.contains('not-read')) {
+        status.classList.add('read');
+        status.classList.remove('not-read');
+        status.textContent = 'read';
+    }
+}
+
+// Display existing books 
+window.addEventListener('DOMContentLoaded', displayBooks);
+// Open Modal
+openDialogForm.addEventListener('click', () => {
+    formDialog.showModal();
+});
+// Close Modal 
+closeDialogForm.addEventListener('click', () => {
+    formDialog.close();
+})
+// Add Book Form 
+addBookForm.addEventListener('submit', addNewBook)
+// Remove Book 
+bookContainer.addEventListener('click', removeBook)
+// Toggle Status Button 
+bookContainer.addEventListener('click', toggleStatus)
